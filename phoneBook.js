2e66e9d9129d90fed58fbaf95aa6bf8ffb55417b
Vasilii <kuznecov_vasilii@.mail.ru>
2015-10-13 01:29:32 +0500

    сделал вывод таблицы и импорт из файла

--- phoneBook.js
+++ phoneBook.js
@@ -19,7 +19,7 @@ module.exports.add = function add(name, phone, email) {
 
 function isValidPhone(phone) {
     var validPhoneRegexp
-        = /^(\+?\d{1,2})? ?(\(\d{3}\)|\d{3}) ?(\d{7}|\d{3}-\d-\d{3}|\d{3} \d \d{3})$/;
+        = /^((\+?\d{1,2})? ?(\(\d{3}\)|\d{3}) ?)?(\d{7}|\d{3}-\d-\d{3}|\d{3} \d \d{3})$/;
     return validPhoneRegexp.test(phone);
 }
 
@@ -64,16 +64,82 @@ module.exports.remove = function remove(query) {
 
 module.exports.importFromCsv = function importFromCsv(filename) {
     var data = require('fs').readFileSync(filename, 'utf-8');
-    var records = data.split('\n');
+    var records = data.split('\r\n');
     var recordsAdded = 0;
     for (var i = 0; i < records.length; i++) {
         if (module.exports.add.apply(this, records[i].split(';'))) {
             recordsAdded++;
         }
     }
-    console.log('Записей добавлено: ' + recordsAdded);
+    console.log('Добавлено контактов: ' + recordsAdded);
 };
 
 module.exports.showTable = function showTable() {
+    var maxLength = {
+        name: 3,
+        phone: 7,
+        email: 5
+    };
+    for (var i = 0; i < phoneBook.length; i++) {
+        var phoneBookRecord = phoneBook[i];
+        if (phoneBookRecord.name.length > maxLength.name) {
+            maxLength.name = phoneBookRecord.name.length;
+        }
+        if (phoneBookRecord.phone.length > maxLength.phone) {
+            maxLength.phone = phoneBookRecord.phone.length;
+        }
+        if (phoneBookRecord.email.length > maxLength.email) {
+            maxLength.email = phoneBookRecord.email.length;
+        }
+    }
+    var table = '';
+    table +=
+        '┌' + repeatSymbol('─', maxLength.name + 2) +
+        '┬' + repeatSymbol('─', maxLength.phone + 2) +
+        '╥' + repeatSymbol('─', maxLength.email + 2) +
+        '┐\n'
+    ;
+    table +=
+        '│ Имя' + repeatSymbol(' ', maxLength.name - 2) +
+        '│ Телефон' + repeatSymbol(' ', maxLength.phone - 6) +
+        '║ email' + repeatSymbol(' ', maxLength.email - 4) +
+        '│\n'
+    ;
+    table +=
+        '├' + repeatSymbol('─', maxLength.name + 2) +
+        '┼' + repeatSymbol('─', maxLength.phone + 2) +
+        '╫' + repeatSymbol('─', maxLength.email + 2) +
+        '┤\n'
+    ;
 
+    for (var i = 0; i < phoneBook.length; i++) {
+        var phoneBookRecord = phoneBook[i];
+        var spacesNeeded = {
+            name: maxLength.name - phoneBookRecord.name.length + 1,
+            phone: maxLength.phone - phoneBookRecord.phone.length + 1,
+            email: maxLength.email - phoneBookRecord.email.length + 1
+        };
+        table +=
+            '│ ' + phoneBookRecord.name + repeatSymbol(' ', spacesNeeded.name) +
+            '│ ' + phoneBookRecord.phone + repeatSymbol(' ', spacesNeeded.phone) +
+            '║ ' + phoneBookRecord.email + repeatSymbol(' ', spacesNeeded.email) +
+            '│\n'
+        ;
+    }
+
+    table +=
+        '└' + repeatSymbol('─', maxLength.name + 2) +
+        '┴' + repeatSymbol('─', maxLength.phone + 2) +
+        '╨' + repeatSymbol('─', maxLength.email + 2) +
+        '┘\n'
+    ;
+    console.log(table);
 };
+
+function repeatSymbol(symbol, number) {
+    var result = '';
+    for (var i = 0; i < number; i++) {
+        result += symbol;
+    }
+    return result;
+}
